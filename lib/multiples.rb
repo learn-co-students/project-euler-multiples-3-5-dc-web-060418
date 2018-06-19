e19783119ca0716aed1c8bdf21db542c32c259c5
github username <github email address>
2018-06-19 09:38:32 -0400

    wills solutions

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
 # Enter your procedural solution here!
+# - If we list all of the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
+# - Find the __sum__ of all the multiples of 3 or 5 below 1000.
+# multiples_of_3_5 = []
+# sum = 0
+# (1..1000).each {|x| multiples_of_3_5 << x if x % 3 == 0 || x % 5 == 0}
+# multiples_of_3_5.each {|x| sum+=x}
+# sum
+
+require 'pry'
+def collect_multiples(limit)
+  multiples_of_3_5 = []
+  (1...limit).each {|x| multiples_of_3_5 << x if x % 3 == 0 || x % 5 == 0}
+  multiples_of_3_5
+end
+
+def sum_multiples(limit)
+  array = collect_multiples(limit)
+  sum = 0
+  array.each {|x| sum+=x}
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,45 @@
-# Enter your object-oriented solution here!
+# # Enter your object-oriented solution here!
+# - Code your procedural solution into the `lib/multiples.rb` file.
+# - Then when you have finished the procedural solution, code your object-oriented solution into the `lib/oo_multiples.rb` file.
+# - Run `learn` until you get all of the RSpec tests to pass.
+
+# Multiples
+#   attributes and method tests
+#     raises ArgumentError when initialized without a limit parameter (FAILED - 1)
+#   #collect_multiples
+#     returns the correct multiples of natural numbers below 10 (FAILED - 2)
+#     returns the correct multiples of natural numbers below 100 (FAILED - 3)
+#   #sum_multiples
+#     returns the correct sum of the multiples of 10 (FAILED - 4)
+#     returns the correct sum of the multiples of 100 (FAILED - 5)
+#   project euler solution
+#     returns the correct solution (FAILED - 6)
+#
+#     multiples_of_3_5 = []
+   # sum = 0
+   # (1..1000).each {|x| multiples_of_3_5 << x if x % 3 == 0 || x % 5 == 0}
+   # multiples_of_3_5.each {|x| sum+=x}
+   # sum
+
+class Multiples
+
+ attr_accessor :limit
+
+ def initialize(limit)
+   @limit = limit
+ end
+
+ def collect_multiples
+   multiples_of_3_5 = []
+   (1...self.limit).each {|x| multiples_of_3_5 << x if x % 3 == 0 || x % 5 == 0}
+   multiples_of_3_5
+ end
+
+
+ def sum_multiples
+   sum = 0
+   array = self.collect_multiples
+   array.each{|x| sum+= x}
+   sum
+ end
+end
