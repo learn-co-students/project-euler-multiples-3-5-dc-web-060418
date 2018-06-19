5b7086a62272f87cb0462b88e785aed3d01bcaa9
jchamberlain909 <jchamberlain909@gmail.com>
2018-06-19 09:38:25 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    multiplier = 1
+    multiples = []
+    loop do
+        if multiplier*5 < limit
+            multiples << multiplier*5
+        end
+        if multiplier*3 < limit
+            multiples << multiplier*3
+        else
+            break
+        end
+        multiplier+=1
+    end
+    multiples.sort.uniq
+end 
+
+
+def sum_multiples limit
+    collect_multiples(limit).reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :limit
+    def initialize (limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        multiplier = 1
+        multiples = []
+        loop do
+            if multiplier*5 < self.limit
+                multiples << multiplier*5
+            end
+            if multiplier*3 < self.limit
+                multiples << multiplier*3
+            else
+                break
+            end
+            multiplier+=1
+        end
+        multiples.sort.uniq
+    end
+
+    def sum_multiples
+        self.collect_multiples.reduce(:+)
+    end
+end
